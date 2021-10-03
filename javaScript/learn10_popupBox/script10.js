3b4f743e532c80dd6bff583b90009ce56b98ca59
Daffa Harizal Fitri <harizal.daffa46@gmail.com>
2021-10-03 07:26:42 +0700

    membuat pengkondisian melalui tahap 1 confirm

--- javaScript/learn10_popupBox/script10.js
+++ javaScript/learn10_popupBox/script10.js
@@ -1,1 +1,6 @@
-confirm('Apakah kamu yakin ingin menghapusnya??');
+var tes = confirm('Apakah kamu yakin ingin menghapusnya??');
+if(tes === true ) {
+    alert('Konsumen menekan tombol true');
+} else if (tes === false ) {
+    alert('Konsumen menekan tombol false');
+}
